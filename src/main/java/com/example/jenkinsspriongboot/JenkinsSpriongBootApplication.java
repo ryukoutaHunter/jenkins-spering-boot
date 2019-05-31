5b6c6b8a137e8d61b70153feae75968ac5bff62a
hunte <haotian.liu_log@baozun.com>
2019-06-01 00:02:16 +0800

    jenkins spring boot

---
+++ src/main/java/com/example/jenkinsspriongboot/JenkinsSpriongBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsspriongboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpriongBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpriongBootApplication.class, args);
+	}
+
+}
